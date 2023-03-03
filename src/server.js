7082c52a9a7f4dd176b6eb11f922471c43408de0
M Yasir Ozdemir <yasiryksmail@gmail.com>
2023-03-03 13:04:33 +0300

    Error Handlers created

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
 import Express from "express";
 import listEndpoints from "express-list-endpoints";
 import productsRouter from "./api/products/index.js";
+import {
+  badRequestHandler,
+  genericErrorHandler,
+  notFoundHandler,
+  unauthorizedHandler,
+} from "./errorHandlers.js";
 import { publicFolderPath } from "./lib/fs-tools.js";
 
 const server = Express();
@@ -10,6 +16,11 @@ server.use(Express.static(publicFolderPath));
 server.use(Express.json());
 
 server.use("/products", productsRouter);
+
+server.use(badRequestHandler);
+server.use(unauthorizedHandler);
+server.use(notFoundHandler);
+server.use(genericErrorHandler);
 
 server.listen(port, () => {
   console.table(listEndpoints(server));
